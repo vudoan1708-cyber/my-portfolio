8699983cdbfb3c9edc14b6bfc0bacb53528be5b4
Vu Doan <vu@voly.com>
2026-05-03 02:34:23 +0100

    Chore: Cleanup

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"components/StarField.jsx -> ./StarFieldCanvas\":{\"id\":\"components/StarField.jsx -> ./StarFieldCanvas\",\"files\":[\"static/chunks/_app-pages-browser_src_components_StarFieldCanvas_jsx.js\"]}}"
